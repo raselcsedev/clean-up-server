39696dc3f142b74191535306ec7bd4b53da7c0ee
raselcsedev <raselcse.dev@gmail.com>
2022-08-21 09:13:51 +0600

    update and delete service

--- index.js
+++ index.js
@@ -25,11 +25,55 @@ async function run() {
       await client.connect();
       const servicesCollection = client.db("cleanUp").collection("service");
 
+      // get-service
       app.get("/service", async (req, res) => {
         const services = await servicesCollection.find({}).toArray();
         console.log(services);
         res.send(services);
       });
+
+      // post-service
+      app.post("/service", async (req, res) => {
+        const data = req.body;
+        const result = await servicesCollection.insertOne(data);
+        res.send(result);
+      });
+
+      // update-service
+      app.put("/service/:id", async (req, res) => {
+        const { id } = req.params;
+        const data = req.body;
+  
+        const filter = { _id: ObjectId(id) };
+        const updateDoc = { $set: data };
+        const option = { upsert: true };
+  
+        const result = await servicesCollection.updateOne(
+          filter,
+          updateDoc,
+          option
+        );
+  
+        res.send(result);
+      });
+
+      // delete-service
+      app.delete("/service/:id", async (req, res) => {
+        const { id } = req.params;
+        const query = { _id: ObjectId(id) };
+        const result = await servicesCollection.deleteOne(query);
+  
+        res.send(result);
+      });
+
+
+      /* app.get("/user/:id", async (req, res) => {
+        const id = req.params;
+
+        const services = await servicesCollection.find({}).toArray();
+        console.log(services);
+        res.send(services);
+      }); */
       
       
     } finally {
